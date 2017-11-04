9311e51aef859930f32e8a1453fd5f75036573c3
Eric Zhao <eric.ye.zhao@gmail.com>
2017-11-04 14:40:33 +1000

    Aysnc part 1

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
-// import courseApi from '../api/mockCourseApi';
-// import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+import courseApi from '../api/mockCourseApi';
+import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
 
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses};
@@ -15,16 +15,16 @@ export function updateCourseSuccess(course) {
   return {type: types.UPDATE_COURSE_SUCCESS, course};
 }
 
-/*export function loadCourses() {
+export function loadCourses() {
   return function(dispatch) {
-    dispatch(beginAjaxCall());
+    //dispatch(beginAjaxCall());
     return courseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
       throw(error);
     });
   };
-}*/
+}
 
 /*export function saveCourse(course) {
   return function (dispatch, getState) {
